a36acf4c40b77934afbe325d30bd7bfff6552e88
David McNamee <david@mcnamee.io>
2024-05-04 16:19:11 -0400

    update site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -67,6 +67,9 @@ const Index = () => {
           transform: "translate(-50%, calc(0px - min(50%, calc(50vh - 5em))))",
         }}
       >
+        <p>
+          You've reached...
+        </p>
         <animated.h1
           className="text-gradient-pink-blue"
           style={{ lineHeight: "1.1", marginBottom: "0.4em", ...titleProps }}
@@ -75,87 +78,39 @@ const Index = () => {
         </animated.h1>
         <div style={{ display: "grid", fontSize: "1.2em" }}>
           <animated.p style={introProps}>
-            Hi! I'm a software developer from Toronto, soon to be graduating
-            from the University of Waterloo!
+            Hi! I'm a software developer in New York, helping to build modern finance at <a href="https://ramp.com">Ramp</a>!
           </animated.p>
           <animated.div style={{ display: "grid", ...remainingProps }}>
-          <p style={{ marginBottom: 0 }}>Here's where I've worked so far:</p>
+          <p style={{ marginBottom: 0 }}>Here's some of the things I've built in the past:</p>
           <div style={{ fontSize: "0.7em", margin: "1em 0" }}>
             <div className="clearfix" style={{ margin: "1em 0" }}>
               <CompanyLogo image="logos/robinhood.png" href="https://robinhood.com"/>{" "}
-              At{" "}
-              <a href="https://robinhood.com">
-                <em>Robinhood</em>
-              </a>
-              , I've been learning all about Payments! I'm helping to integrate
-              the Robinhood system with Stripe so that you can fund your
-              Robinhood account via debit card.
+              An instant debit card payment rail for <a href="https://robinhood.com">Robinhood</a> -- fund your account faster than you can say "buy the dip"!
             </div>
             <div className="clearfix" style={{ margin: "1em 0" }}>
               <CompanyLogo href="https://meta.com" image="logos/meta.jpg" />{" "}
-              At{" "}
-              <a href="https://meta.com">
-                <em>Meta</em>
-              </a>
-              , I dived deep into Intel SGX -- a hardware-level technology that
-              encrypts memory at runtime, and protects you from high-privileged
-              malware.
+              A C++ security framework for encrypting memory at runtime on ultra-sensitive <a href="https://meta.com">Meta</a> services
             </div>
             <div className="clearfix" style={{ margin: "1em 0" }}>
             <CompanyLogo href="https://wish.com" image="logos/wish.jpg" />{" "}
-              At{" "}
-              <a href="https://wish.com">
-                <em>Wish</em>
-              </a>
-              , I rebuilt the Merchant Product API (v3), giving it both a speed
-              boost and better DX!
+              A faster, more intuitive Product API for <a href="https://wish.com">Wish</a> merchants!
             </div>
             <div className="clearfix" style={{ margin: "1em 0" }}>
             <CompanyLogo href="https://lazertechnologies.com" image="logos/lazer.png" />{" "}
-              At{" "}
-              <a href="https://lazertechnologies.com">
-                <em>Lazer Technologies</em>
-              </a>
-              , I created the pilot version of the AirMiles Shopify App.
+              A pilot version of the AirMiles Shopify App at <a href="https://lazertechnologies.com">Lazer Technologies</a>, and the first iteration of RBC's <a href="https://www.gocanadaunited.ca/">Canada United</a> website!
             </div>
             <div className="clearfix" style={{ margin: "1em 0" }}>
             <CompanyLogo href="https://snapcommerce.com" image="logos/snapcommerce.png" backgroundColor="#FDFEFE" />{" "}
-              At{" "}
-              <a href="https://snapcommerce.com">
-                <em>SnapTravel</em>
-              </a>
-              , I built a library to connect directly to hotel-deal suppliers!
-              (as opposed to using an aggregator service)
+              A microservice for direct-to-supplier hotel bookings at <a href="https://snapcommerce.com">SnapTravel</a>
             </div>
           </div>
-          <p style={{ marginBottom: 0 }}>
-            When I'm not working, I like to code for fun!
+          <p>
+            I'm not currently looking for a job... this website is really an excuse to play around with fun particle effects{" "}
           </p>
-          <ul style={{ listStyle: "none", fontSize: "0.7em" }}>
-            <li style={{ margin: "1em 0" }}>
-              <a href="https://github.com/team-dj/starburst">
-                <strong>Starburst</strong>
-              </a>{" "}
-              is a framework that will (someday soon) allow you to build and
-              connect microservices seamlessly. So smooth, it'll feel like
-              you're building a monolith!
-            </li>
-            <li style={{ margin: "1em 0" }}>
-              <a href="https://devpost.com/software/groupshot">
-                <strong>groupShot</strong>
-              </a>{" "}
-              is a video-chat app that I built for a hackathon. It crops you out
-              and overlays your video feed with that of your partner's, so that
-              you can feel like you're together even when socially distanced.
-            </li>
-          </ul>
+          
           <p>
             Feel free to reach out to me on{" "}
-            <a href="https://www.linkedin.com/in/david-mcnamee/">LinkedIn</a>,{" "}
-            <a href="https://github.com/davidmcnamee">Github</a>, or{" "}
-            <a href="https://discord.com">
-              Discord [davidmcnamee#3535]
-            </a>{" "}
+            <a href="https://www.linkedin.com/in/david-mcnamee/">LinkedIn</a>{" "}
             :)
           </p>
           </animated.div>
